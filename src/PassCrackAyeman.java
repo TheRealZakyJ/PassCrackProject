be28e70d7d11d9cf64d4e3ad27da41b3095e14dc
100000561 <100000561@ccisd.net>
2022-11-17 10:50:07 -0600

    Modified the PassCrackAyeman File, added txt reader

--- src/PassCrackAyeman.java
+++ src/PassCrackAyeman.java
@@ -1,2 +1,13 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+import
+
 public class PassCrackAyeman {
+    public static void main(String[] args) {
+        //Scanner s = new Scanner(new File("words.txt"));
+        //System.out.println(s.next());
+        for(String line: FileUtils.readLines("10k-most-common.txt"))
+            System.out.println(line);
+    }
 }
--- src/PassCracking.java
+++ src/PassCracking.java
@@ -1,7 +1,7 @@
 public class PassCracking {
-
+//comment
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("dfd");
     }
 
 }
