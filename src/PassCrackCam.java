cb791ba535da55a519a33dceb24b332305c5972e
100050781 <camdorchester777@gmail.com>
2022-12-07 10:36:22 -0600

    PassCrackCam: feat: Finished MD5 hash

--- src/PassCrackCam.java
+++ src/PassCrackCam.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.math.BigInteger;
+import bcrypt;
+import jBCrypt;
 
 public class PassCrackCam {
 
@@ -21,81 +23,38 @@ public class PassCrackCam {
 
     public static void main(String[] args) throws NoSuchAlgorithmException{
         // System.out.println("Your first argument is: "+args[0]);
-        //password=args[0];
-        password = "monk";
+        password=args[0];
+        //password = "camoin";
         System.out.println(getMd5(password));
-        /*passType = "brute";
-
-        if(passType.equals("brute")) {
-
-            while (!solved) {
-                // guessPass.append(charArr[0]);
-                gPass.add(Character.toString(charArr[0]));
-                //System.out.println(guessPass.length()+"len");
-                for (int i = 0; i < gPass.size()-1; i++) {
-                    for (int j = 0; j < charLen; j++) {
-                        //guessPass.setCharAt(i, charArr[j]);
-                        gPass.set(i,Character.toString(charArr[j]));
-                        //System.out.println(gPass.toString() + "forst" + i + j);
-                        solved = brute(i + 1);
-                        //System.out.println("sec");
-                    }
-                }
-            }
-        }
-        //PassCrackCam encrypter = new PassCrackCam();
-
-         */
-
 
 
     }
 
-    public static boolean brute(int index) {
-        for (int i = 0; i < charLen; i++) {
-            // guessPass.setCharAt(index, charArr[i]);
-            gPass.set(index,Character.toString(charArr[i]));
-            System.out.println(String.join("",gPass));
-           /* if (guessPass.toString().equals(password)) {
-                System.out.println("Here is the password: " + guessPass);
-                solved = true;
-                return true;
-                //break;
-                //System.exit(0);
-            }
 
-            */
-            if (String.join("",gPass).equals(password)) {
-                System.out.println("Here is the password: " + String.join("",gPass));
-                solved = true;
-                //return true;
-                //break;.ja
-                System.exit(0);
-            }
-            /*if (index < guessPass.length()-1) {
-                brute(index + 1);
-            }
-
-             */
-            if (index < gPass.size()-1) {
-                brute(index + 1);
-            }
-        }
-        //System.out.println(guessPass.toString() + "end");
-        return false;
-    }
 
     public static String getMd5(String input) throws NoSuchAlgorithmException{
         MessageDigest md = MessageDigest.getInstance("MD5");
 
         byte[] digestion = md.digest(input.getBytes());
+        byte[] messageDigest = md.digest(input.getBytes());
 
-        BigInteger IIIINT = new BigInteger(digestion);
+        BigInteger IIIINT = new BigInteger(1, messageDigest);
 
-        return IIIINT.toString();
+        String hashtext = IIIINT.toString(16);
+
+
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
     }
 
+    public static String bcrypt(String input) {
+
+        return null;
+    }
 
 }
+
 
 
