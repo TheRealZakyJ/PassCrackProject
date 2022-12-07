e3231ac8dc0edfcae810a72d3f9be48b46780362
100000561 <100000561@ccisd.net>
2022-12-07 10:13:03 -0600

    PassCrackZak: feat: Tested code and deleted unneccesary code.
    PassCrackAyeman:
    feat:Tested code and completed dict hack.

--- src/PassCrackAyeman.java
+++ src/PassCrackAyeman.java
@@ -20,49 +20,20 @@ public class PassCrackAyeman {
            String[] list1 = new String[10000];
            while (s.hasNext()){
                String line = s.nextLine();
-               System.out.println(line);
                list.add(line);
-               //list[]
+
 
            }
-           /*for(int i=0;i< list1.length;i++){
-               String line = s.nextLine();
-               System.out.println(line);
-               list1[i]=line;
-           }
 
-            */
-           //s.close();
            wordList=list;
-           //Arrays.toString(list1);
-           /*
-           File f = new File("10k-most-common.txt");
-           try{
-               wordList = get_arraylist_from_file(f);
-               //for(int x = 0; x < wordList.size(); x++){
-               // System.out.println(wordList.get(x));
-               // }
 
-
-           } catch (FileNotFoundException e) {
-               e.printStackTrace();
-           }
-
-            */
-
-           //Scanner s = new Scanner(new File("10k-most-common.txt"));
-           //System.out.println(s.next());
-
-           //for(String line: FileUtils.readLines("10k-most-common.txt"))
-           //    System.out.print(line);
            password = args[0];
            passType = args[1];
 
-           //System.out.println("dn");
            if(passType.equals("dict")) {
                dict(password);
            }
-           //System.out.println("don1");
+
        }
 
     public static ArrayList<String> get_arraylist_from_file(File f) throws FileNotFoundException {
@@ -80,7 +51,7 @@ public class PassCrackAyeman {
 
     public static void dict(String pass){
         if(wordList.contains(pass)){
-            System.out.println(wordList.indexOf(pass));
+            System.out.println(wordList.get(wordList.indexOf(pass)));
         }else{
             System.out.println("Not in password list.");
         }
